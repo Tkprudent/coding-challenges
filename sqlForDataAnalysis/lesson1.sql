066d3e79ba78450b274f60dcb9a5648ad6ab85ca
Kazeem Tijani <37423235+Tkprudent@users.noreply.github.com>
2020-02-14 01:21:45 -0500

    Update lesson1.sql

--- sqlForDataAnalysis/lesson1.sql
+++ sqlForDataAnalysis/lesson1.sql
@@ -1,3 +1,14 @@
+/* Every query will have at least a SELECT and FROM statement. 
+The SELECT statement is where you put the columns for which you would like to show the data. 
+The FROM statement is where you put the tables from which you would like to pull data.
+*/
+
+/* Write your own query to select only the id, account_id, and occurred_at 
+columns for all orders in the orders table.*/
+
+SELECT id, account_id, occurred_at
+FROM orders;
+
 /* Write a query that limits the response to only the 
 first 10 rows and includes the date, account_id and 
 channel fiels in the web_events table.
@@ -6,3 +17,4 @@ channel fiels in the web_events table.
 SELECT occurred_at, account_id,channel
 FROM web_events
 LIMIT 10;
+
