25f4b295ec4e24dcc893350e73bc87abd33ea4ee
Kazeem Tijani <37423235+Tkprudent@users.noreply.github.com>
2020-02-15 10:20:49 -0500

    Update lesson1.sql

--- sqlForDataAnalysis/lesson1.sql
+++ sqlForDataAnalysis/lesson1.sql
@@ -9,19 +9,31 @@ columns for all orders in the orders table.*/
 SELECT id, account_id, occurred_at
 FROM orders;
 
-/* Write a query that limits the response to only the 
-first 10 rows and includes the date, account_id and 
-channel fiels in the web_events table.
+/* Write a query that to fetch rows and includes the date, account_id and 
+channel fields in the web_events table.
 */
 
 SELECT occurred_at, account_id,channel
-FROM web_events
-LIMIT 10;
+FROM web_events;
 
 /*Write a query to return the 10 earliest orders in the orders table.
  Include the id, occurred_at, and total_amt_usd*/
 
  SELECT id, occurred_at, total_amt_usd
  FROM orders
- ORDER BY occurred_at
- LIMIT 10;
+ ORDER BY occurred_at;
+
+ /*Provide a table that provides the region for each sales_rep along with their associated accounts. 
+ This time only for the Midwest region. Your final table should include three columns: 
+ the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.*/
+
+ SELECT r.name Region, s.name SalesRep, a.name Account 
+ FROM sales_reps AS s
+ JOIN region AS r
+ ON s.region_id = r.id
+ JOIN accounts As a
+ ON a.sales_rep_id = s.id
+ WHERE r.name = 'Midwest'
+ ORDER BY a.name;
+
+ 
