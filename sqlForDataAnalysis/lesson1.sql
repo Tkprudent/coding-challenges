d8c0e50052cbf77cac996b749ebb5205d86b8df1
Kazeem Tijani <37423235+Tkprudent@users.noreply.github.com>
2020-02-18 23:22:15 -0600

    Update lesson1.sql

--- sqlForDataAnalysis/lesson1.sql
+++ sqlForDataAnalysis/lesson1.sql
@@ -55,3 +55,16 @@ WHERE w.channel = 'facebook'
 GROUP BY a.id, a.name, w.channel
 ORDER BY use_of_channel DESC
 LIMIT 1;
+
+/*Which accounts used facebook as a channel to contact customers more than 6 times?*/
+
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+HAVING COUNT(*) > 6 AND w.channel = 'facebook'
+ORDER BY use_of_channel;
+
+/*How many accounts spent less than 1,000 usd total across all orders?*/
+
